2e2ea392004c30a5e37120c8420165a25382a4f1
Sai Sampath Mahajan <sai.mahajan.17@mountblue.tech>
2021-09-19 20:25:06 +0530

    whole app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { Footer } from "./MyComponents/Footer";
+import React, { useState, useEffect } from "react";
+import { About } from "./About";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let initTodo;
+    if (localStorage.getItem("todos") === null) {
+        initTodo = [];
+    } else {
+        initTodo = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    const onDelete = (todo) => {
+        console.log("delete it", todo);
+        setTodos(
+            todos.filter((e) => {
+                return e !== todo;
+            })
+        );
+        localStorage.getItem("todos");
+    };
+
+    const addTodo = (title, desc) => {
+        console.log("I'm adding this todo", title, desc);
+        let sno;
+        if (todos.length === 0) {
+            sno = 1;
+        } else {
+            sno = todos[todos.length - 1].sno + 1;
+        }
+
+        const myTodo = {
+            sno: sno,
+            title: title,
+            desc: desc,
+        };
+        setTodos([...todos, myTodo]);
+        console.log(myTodo);
+    };
+
+    const [todos, setTodos] = useState(initTodo);
+    useEffect(() => {
+        localStorage.setItem("todos", JSON.stringify(todos));
+    }, [todos]);
+
+    return (
+        <>
+            <Router>
+                <Header title="My Todos List" searchBar={true} />
+                <Switch>
+                    <Route
+                        exact path="/"
+                        render={() => {
+                            return (
+                                <>
+                                    `<AddTodo addTodo={addTodo} />
+                                    <Todos todos={todos} onDelete={onDelete} />
+                                </>
+                            );
+                        }}
+                    ></Route>
+                    <Route exact path="/about">
+                        <About />
+                    </Route>
+                </Switch>
+                <Footer />
+            </Router>
+        </>
+    );
 }
 
 export default App;
